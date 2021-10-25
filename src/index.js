55c730262db2feaa0d5a324fef015303f72cea88
Beautifullagency <Javiercossio@beautifullagency.com>
2021-10-25 13:21:14 -0300

    make it better

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 let client_id = '3MVG9LBJLApeX_PAOL8P8mOUd4nVt3vEFrBWR3A_CIVRpm9XoV3Vs75EgJXBm123XIOoNlk.3ATAKxU5x0rIn'
 let client_secret = 'CD676C6964227D3163149B6BD77C30EBFBDEBA05DF93F759F9FA873E59219C22'
-let redirect_uri = 'http://localhost:4000/auth/handle_decision'
+let redirect_uri = 'https://server-sf.herokuapp.com/auth/handle_decision'
 
 
 
