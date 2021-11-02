7a084407a4eff131b8d61b359a1087850d8c9dd4
Beautifullagency <Javiercossio@beautifullagency.com>
2021-11-02 16:16:19 -0300

    test

--- src/index.js
+++ src/index.js
@@ -61,8 +61,7 @@ app.get('/auth/handle_decision', async (req, res) => {
     let data = payload
     //console.log('Respuesta: ', data)
     res.cookie('sheet', data.access_token, { maxAge: data.issued_at })
-    res.cookie('clean_sheet', data.refresh_token)
-    res.send('')
+    res.cookie('clean_sheet', data.refresh_token).send()
     res.redirect('https://d3v-testing.zendesk.com/agent/apps/local-app?zat=true')
   })
 })
