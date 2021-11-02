f44eb0617ca1ca66f8c6584d3714a9859d3c3164
Beautifullagency <Javiercossio@beautifullagency.com>
2021-11-02 17:08:07 -0300

    test

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ app.get('/auth/salesforce', async (req, res) => {
 })
 app.get('/auth/token', async (req, res) => {
 
-  let cookie = req.cookies.sheet
+  let cookie = req.signedCookies
   //todasLasCookies = document.cookie
   console.log(cookie)
   res.json(cookie)
