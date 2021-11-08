2d15a0214fa8962f50c18566577298908f771145
Beautifullagency <Javiercossio@beautifullagency.com>
2021-11-08 16:27:46 -0300

    test

--- src/index.js
+++ src/index.js
@@ -56,10 +56,11 @@ app.get('/auth/handle_decision', async (req, res) => {
     code: authorizationCode,
   }, function (error, payload) {
     let data = payload
-    console.log('Respuesta: ', data)
     res.cookie('sheet', data.access_token, { maxAge: data.issued_at })   
     res.cookie('clean_sheet', data.refresh_token)
-    //res.redirect('https://d3v-testing.zendesk.com/agent/apps/local-app?zat=true')
+    res.send('Cookie have been saved successfully');
+    next()
+    res.redirect('https://d3v-testing.zendesk.com/agent/apps/local-app?zat=true')
     res.send("<script>window.close();</script >")
   })
 })
