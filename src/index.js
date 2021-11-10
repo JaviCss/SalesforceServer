168978f33bc0cfca9c3ccaa5c6f1878cf8bd8c4a
Beautifullagency <Javiercossio@beautifullagency.com>
2021-11-10 20:09:06 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const oauth2 = require('salesforce-oauth2')
 const cookieParser = require('cookie-parser')
-const fetch = require('node-fetch')
+import fetch from 'node-fetch';
 
 
 ///////
