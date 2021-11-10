1845ec5552e9ca58b7d10f2a5d595c69e4130e3d
Beautifullagency <Javiercossio@beautifullagency.com>
2021-11-10 16:04:15 -0300

    test

--- src/index.js
+++ src/index.js
@@ -59,8 +59,8 @@ app.get('/auth/handle_decision', async (req, res) => {
     code: authorizationCode,
   }, function (error, payload) {
     let data = payload
-    res.cookie('sheet', data.access_token, { maxAge: data.issued_at, httpOnly: true, sameSite:'none'})
-    res.cookie('clean_sheet', data.refresh_token, { httpOnly: true, sameSite:'none'}) 
+    res.cookie('sheet', data.access_token, { maxAge: data.issued_at, httpOnly: true})
+    res.cookie('clean_sheet', data.refresh_token, { httpOnly: true, sameSite: false}) 
     res.send("<script>window.close();</script >")
     res.end()
    
