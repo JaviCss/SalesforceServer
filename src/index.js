4dc76e849be01523665679c748ff43b5d0852bf3
Beautifullagency <Javiercossio@beautifullagency.com>
2021-11-15 19:09:50 -0300

    fixes

--- src/index.js
+++ src/index.js
@@ -95,9 +95,9 @@ app.get('/auth/handle_decision', async (req, res) => {
     let time =  new Date(new Date().getTime()+1*3600*1000).toGMTString()
     console.log('Tiempo: ',time)
     let time_refresh =  new Date(new Date().getTime()+30*3600*1000).toGMTString()
-    res.cookie('sheet', data.access_token, { expire: time, httpOnly: true, sameSite: 'none', secure: true })
-    res.cookie('clean_sheet', data.refresh_token, { expire: time_refresh, httpOnly: true, sameSite: false, sameSite: 'none', secure: true })
-    res.cookie('id_sheet', data.instance_url, { expire: time, httpOnly: true, sameSite: false, sameSite: 'none', secure: true })
+    res.cookie('sheet', data.access_token, { expires: time, httpOnly: true, sameSite: 'none', secure: true })
+    res.cookie('clean_sheet', data.refresh_token, { expires: time_refresh, httpOnly: true, sameSite: false, sameSite: 'none', secure: true })
+    res.cookie('id_sheet', data.instance_url, { expires: time, httpOnly: true, sameSite: false, sameSite: 'none', secure: true })
 
 
 
