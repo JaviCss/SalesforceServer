38ceeacdb1c4e372ffbdfd030cc61cccef2d8cb2
Beautifullagency <Javiercossio@beautifullagency.com>
2021-11-19 17:56:16 -0300

    fixes

--- src/index.js
+++ src/index.js
@@ -38,8 +38,8 @@ app.get('/auth/salesforce', async (req, res) => {
     client_id: consumeri,
     scope: ' api web refresh_token', // 'id api web refresh_token'
   })
-  res.cookie('consumer_id_sheet', 'tuvieja', { maxAge: ageLong, httpOnly: true, sameSite: 'none', secure: true })
-    res.end()
+ res.send(`${uri}`)
+  res.end()
 })
 
 //TOKEN
@@ -94,7 +94,7 @@ app.get('/auth/handle_decision', async (req, res) => {
     res.cookie('sheet', data.access_token, { maxAge: ageLong, httpOnly: true, sameSite: 'none', secure: true })
     res.cookie('clean_sheet', data.refresh_token, { maxAge: ageLong, httpOnly: true, sameSite: false, sameSite: 'none', secure: true })
     res.cookie('id_sheet', data.instance_url, { maxAge: ageLong, httpOnly: true, sameSite: false, sameSite: 'none', secure: true })
-
+    res.cookie('consumer_id_sheet', client_id, { maxAge: ageLong, httpOnly: true, sameSite: 'none', secure: true })
 
 
     res.send("<script>window.close();</script >")
