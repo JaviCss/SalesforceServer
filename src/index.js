9c50314e07456be59849087abd9f31f77b8d3156
Beautifullagency <Javiercossio@beautifullagency.com>
2021-11-30 19:08:50 -0300

    clg

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ app.get('/auth/salesforce', async (req, res) => {
   var uri = oauth2.getAuthorizationUrl({
     redirect_uri: redirect_uri,
     client_id: id,
-    scope: ' openid api web refresh_token', // 'id api web refresh_token'
+    scope: 'api web refresh_token', // 'id api web refresh_token'
   })
   res.cookie('consumer_id_sheet', id, { maxAge: ageLong, httpOnly: true, sameSite: 'none', secure: true })
  //res.send(`${uri}`)
