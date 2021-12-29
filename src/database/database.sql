457ea67e359152111f4f52b59de2adc956954564
Beautifullagency <Javiercossio@beautifullagency.com>
2021-12-29 19:13:36 -0300

    fix

--- src/database/database.sql
+++ src/database/database.sql
@@ -6,8 +6,12 @@ CREATE DATABASE webserversalesforce ;
 
 
 
-CREATE TABLE usuarios ( id SERIAL PRIMARY KEY, consumeri VARCHAR(100) , consumers VARCHAR(100) ,tokenrefresh VARCHAR(100), dominio TEXT);
+CREATE TABLE usuarios ( id SERIAL PRIMARY KEY, consumeri VARCHAR(100) , consumers VARCHAR(100) ,tokenrefresh VARCHAR(100), domain TEXT);
 
 INSERT INTO usuarios(consumeri, consumers, tokenrefresh, domain) VALUES
         ('consumeri', 'consumers', 'tokenrefresh', 'joaquin.decima@gmail.com'),
         ('39423029', 'Pato', 'Decima', 'patojad96@gmail.com');
+
+\dt
+
+DROP TABLE IF EXISTS usuarios CASCADE        
